18963180caab0ceed0bb67a2dc0afedf7507aa78
John Ramirez <jvramirez13@gmail.com>
2019-09-25 21:01:38 +0000

    changed Dockerfile

--- upload.py
+++ upload.py
@@ -20,4 +20,4 @@ def uploader():
       return returnString + "\n You got " + str(retcode) + " out of 2 correct!"
 
 if __name__ == '__main__':
-   app.run(host = "0.0.0.0", port = 80, debug = True)
+   app.run(host = "0.0.0.0",port = 80, debug = True)
